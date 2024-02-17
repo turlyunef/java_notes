a93b3c91961f21200aae0081f6bed85153c97ee0
turlyunef <turlyunef@mail.ru>
2024-02-17 09:55:41 +0700

    simple coroutine example was expanded

--- Kotlin-questions/src/main/kotlin/coroutines/SimpleCoroutinDemo.kt
+++ Kotlin-questions/src/main/kotlin/coroutines/SimpleCoroutinDemo.kt
@@ -4,13 +4,24 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
 suspend fun main() = coroutineScope {
     println("Hello from main")
+    var simpleInt = 123
     launch {
+        simpleInt = 456
         println("Hello from coroutine. Wait..")
+
         delay(4000)
+        simpleInt = 789
         println("Bye from coroutine!")
     }
-    println("Again in main")
+    println("Again in main and simpleInt = $simpleInt")
+    println("Wait...")
+
+    delay(200)
+    println("Again in main and simpleInt = $simpleInt")
+    println("Wait...")
+
+    delay(3810)
+    println("Again in main and simpleInt = $simpleInt")
 }
